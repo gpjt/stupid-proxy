a2f4dc2ee47fe542c6369d791e0b8b54c06ac4d5
Giles Thomas <giles@giles.net>
2013-07-11 14:21:06 +0000

    Removed debug logging -- massive speed improvement!  (Thanks, Hansel)

--- proxy.go
+++ proxy.go
@@ -7,9 +7,6 @@ import "net"
 import "strings"
 
 func doHTTPProxy(downstream net.Conn) {
-    println("In doHTTPProxy")
-
-    println("Reading headers")
     reader := bufio.NewReader(downstream)
     hostname := ""
     readLines := list.New()
@@ -20,11 +17,9 @@ func doHTTPProxy(downstream net.Conn) {
             return
         }
         line := string(bytes)
-        println("Read ", line)
         readLines.PushBack(line)
         if strings.HasPrefix(line, "Host: ") {
             hostname = strings.TrimPrefix(line, "Host: ")
-            println("Found host header:", hostname)
             break
         }
     }
@@ -33,22 +28,17 @@ func doHTTPProxy(downstream net.Conn) {
         return
     }
 
-    println("Connecting to downstream")
     upstream, error := net.Dial("tcp", hostname + ":80")
     if error != nil {
         println("Couldn't connect to upstream", error)
         return
     }
-    println("Connected to downstream", downstream, upstream)
 
-    println("Replaying headers we've already received")
     for element := readLines.Front(); element != nil; element = element.Next() {
         line := element.Value.(string)
         upstream.Write([]byte(line))
         upstream.Write([]byte("\n"))
-        println("Sent ", line)
     }
-    println("Replayed headers, connecting for full proxying")
 
     go io.Copy(upstream, reader)
     go io.Copy(downstream, upstream)
@@ -68,7 +58,6 @@ func main() {
             return
         }
 
-        println("New connection", connection)
         go doHTTPProxy(connection)
     }
 }
